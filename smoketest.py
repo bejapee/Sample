4082df9c1e677bd8798fa375354fd3e3fcf5b20a
Bert Peeters <bertpeeters@iMac-van-Bert.local>
2020-08-13 11:12:40 +0200

    Initial thoughts about smoketest script

---
+++ smoketest.py
@@ -0,0 +1,6 @@
+''' Smoketest is a python scripts that checks whether
+    the program has been properly set up
+'''
+
+# Not an easy task
+# See Mac Collateral\DevOps\GIT - Notebook and search for Smoke-Test 
