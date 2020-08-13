9727ca6f8c4e922d3b974fd23e445f25e1df9e75
Bert Peeters <bertpeeters@iMac-van-Bert.local>
2020-08-13 10:39:27 +0200

    Setup.py v.0.1 added. This version causes  dependencies to be installed.

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-# Put your setup script here
+''' Setup script for sample project
+    Run this script to start the program
+'''
+
+# Use subprocess to run an external command;
+# The function 'call' works in Python 2.7;
+# But in Python 3 it has been superceded by 'run'.
+# Note that you must split the command line command into elements;
+# That you put in a list; making sure there are no blanks.
+
+import subprocess
+
+subprocess.call(["pip", "install", "-r", "requirements.txt"])
